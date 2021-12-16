83231604e2c25b7e86e9ecc6f2e50f4c520fb1c4
preetam mondal <preetammondal78@gmail.com>
2021-12-16 18:24:45 +0530

    login commit

---
+++ components/Login.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+
+function Login() {
+  const { authenticate } = useMoralis();
+
+  return (
+    <div className="bg-black relative text-white">
+      <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-6">
+        <Image
+          className="rounded-full overflow-hidden object-cover"
+          src="https://www.freeiconspng.com/uploads/unlock-icon-18.png"
+          height={200}
+          width={200}
+        />
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse hover:scale-110"
+        >
+          Login to Metaverse
+        </button>
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          src="https://wallpaperaccess.com/full/3648367.jpg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["wallpaperaccess.com", "www.freeiconspng.com"],
+  },
+};
